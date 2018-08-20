1e304115a643656af039b58108e15b3545e60d0a
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-08-20 13:01:19 -0500

    added: styled-component card div

--- src/Drag.js
+++ src/Drag.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { Gesture } from 'react-with-gesture';
 import { Spring, animated, interpolate } from 'react-spring';
 import { Card } from 'Elements';
 
-const AnimCard = Card.withComponent(animated.div)
+const AnimCard = Card.withComponent(animated.div);
+
+const DragCard = AnimCard.extend`
+  position: absolute;
+  width: 320px;
+  height: 300px;
+`;
+
+const CardContainer = styled(animated.div)`
+  position: relative;
+  background: #ccc;
+  max-width: 350px;
+  height: 330px;
+  margin: 0 auto;
+  border-radius: 5px;
+`;
 export default class Drag extends Component {
   render() {
     return (
@@ -17,11 +33,33 @@ export default class Drag extends Component {
             immediate={name => down && name === 'x'}
           >
           {({ x }) => (
-            <AnimCard style={{
-              transform: x.interpolate(x => `translateX(${x}px)`)
+           <CardContainer style={{
+              background: x.interpolate({
+                  range: [-300, 300],
+                  output: ['#ff1C68', '#14D790'],
+                  extrapolate: 'clamp'
+                })
             }}>
-              <h1>Drag</h1>
-            </AnimCard>
+              <DragCard style={{
+                opacity: x.interpolate({
+                    range: [-300, -100],
+                    output: [0, 1],
+                    extrapolate: 'clamp'
+                  }),
+                transform: interpolate(
+                  [x,
+                    x.interpolate({
+                    range: [-300, 300],
+                    output: [-45, 45],
+                    extrapolate: 'clamp'
+                  })
+                  ],
+                  (x, rotate) => `translateX(${x}px) rotate(${rotate}deg)`
+                )
+              }}>
+                <h1>Drag</h1>
+              </DragCard>
+            </CardContainer>
           )}
           </Spring>
         )}
