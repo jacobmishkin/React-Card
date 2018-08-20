bf99903e4eb8d8eff51da026eb150a09033c862b
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-08-20 08:56:03 -0500

    react-spring native animations

--- src/Elements/Modal.js
+++ src/Elements/Modal.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import {Transition, animated} from 'react-spring';
 
 import {Portal, absolute, relative} from 'Utils';
 import {Card} from './Cards';
@@ -10,17 +11,39 @@ export default class Modal extends Component {
     const {children, on, toggle} = this.props;
     return (
       <Portal>
-        {on && (
-          <ModalWrapper>
-            <ModalCard>
+      <Transition
+        native
+        config={{
+          tension: 280,
+          friction: 60
+        }}
+        from={{opacity: 0, bgOpacity: 0, y: -50}}
+        enter={{opacity: 1, bgOpacity: 0.5, y: 0}}
+        leave={{opacity: 0, bgOpacity: 0, y: 50}}
+      >
+        {on &&
+        (styles => (
+          <ModalWrapper style={{ ...styles }}>
+            <ModalCard style={{
+              transform: styles.y.interpolate(
+                y => `translate3d(0, ${y}px, 0)`,
+              ),
+              ...styles
+            }}>
               <CloseButton onClick={toggle}>
                 <Icon name="close" />
               </CloseButton>
               <div>{children}</div>
             </ModalCard>
-            <Background onClick={toggle} />
+            <Background
+            style={{ opacity: styles.bgOpacity.interpolate(
+              bgOpacity => bgOpacity
+              )
+            }}
+            onClick={toggle} />
           </ModalWrapper>
-        )}
+        ))}
+        </Transition>
       </Portal>
     );
   }
@@ -35,7 +58,9 @@ const ModalWrapper = styled.div`
   align-items: center;
 `;
 
-const ModalCard = Card.extend`
+const AnimCard = Card.withComponent(animated.div)
+
+const ModalCard = AnimCard.extend`
   ${relative()}
   z-index: 10;
   margin-bottom: 100px;
@@ -52,7 +77,7 @@ const CloseButton = styled.button`
   cursor: pointer;
 `;
 
-const Background = styled.div`
+const Background = styled(animated.div)`
   ${absolute({})} width: 100%;
   height: 100%;
   background: black;
