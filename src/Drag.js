71f518d55a90f68af93503099c8f1f16813a780b
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-08-20 10:29:13 -0500

    added: native support for animation

--- src/Drag.js
+++ src/Drag.js
@@ -3,23 +3,25 @@ import { Gesture } from 'react-with-gesture';
 import { Spring, animated, interpolate } from 'react-spring';
 import { Card } from 'Elements';
 
+const AnimCard = Card.withComponent(animated.div)
 export default class Drag extends Component {
   render() {
     return (
       <Gesture>
         {({down, xDelta }) => (
           <Spring
+            native
             to={{
               x: down ? xDelta : 0
             }}
             immediate={name => down && name === 'x'}
           >
           {({ x }) => (
-            <Card style={{
-              transform: `translateX(${x}px)`
+            <AnimCard style={{
+              transform: x.interpolate(x => `translateX(${x}px)`)
             }}>
               <h1>Drag</h1>
-            </Card>
+            </AnimCard>
           )}
           </Spring>
         )}
